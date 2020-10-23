d3e252a5c23f39f9de89269088bdcc4a98ccdedc
tk072013 <Tarun.Kolla@cerner.com>
2020-10-23 15:58:23 -0500

    w.e

--- demos/browser/server.js
+++ demos/browser/server.js
@@ -12,7 +12,7 @@ const { v4: uuidv4 } = require('uuid');
 const meetingTable = {};
 
 // Use local host for application server
-const host = '127.0.0.1:8080';
+const host = '0.0.0.0:8080';
 
 // Load the contents of the web application to be used as the index page
 const indexPage = fs.readFileSync(`dist/${process.env.npm_config_app || 'meetingV2'}.html`);
